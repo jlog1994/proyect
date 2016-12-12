4cb263e742343a16d5e8fc4ab692d3be7f5d342d
Pedro <peter221602@gmail.com>
2016-12-12 01:21:04 -0600

    Conexion hosting y BD

--- class/conexion.php
+++ class/conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-$conexion = mysql_connect("localhost", "root", "12345")
+$conexion = mysql_connect("mysql.hostinger.mx", "u243481060_root", "qwerty")
         or die("Error de conexion" . mysql_error());
 
 if (!$conexion) {
     header("location:verificacion.php?error=Error en la Conexion de la DB");
     exit();
 }
-$db = mysql_select_db("chirris")
+$db = mysql_select_db("u243481060_hola")
         or die("Error al seleccionar la base de datos" . mysql_error());
 if (!$db) {
     header("location:verificacion.php?error=Error al Seleecionar la DB");
